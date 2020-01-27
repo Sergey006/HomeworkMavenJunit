bca04ab6216cd09f93e0b8d74fa69ce11befd6df
Sergey <sergeygreblov@gmail.com>
2020-01-27 18:54:54 +0300

    Parametrized test

--- src/test/java/SberbankTest.java
+++ src/test/java/SberbankTest.java
@@ -2,21 +2,60 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+@RunWith(Parameterized.class)
 public class SberbankTest {
+
     public static WebDriver webDriver = null;
+
+    @Parameterized.Parameter(0)
+    public String surname;
+    @Parameterized.Parameter(1)
+    public String name;
+    @Parameterized.Parameter(2)
+    public String birthdayDate;
+    @Parameterized.Parameter(3)
+    public String lastName;
+    @Parameterized.Parameter(4)
+    public String firstName;
+    @Parameterized.Parameter(5)
+    public String middleName;
+    @Parameterized.Parameter(6)
+    public String passportBirthdayDate;
+    @Parameterized.Parameter(7)
+    public String passportSeries;
+    @Parameterized.Parameter(8)
+    public String passportNumber;
+    @Parameterized.Parameter(9)
+    public String documentDate;
+    @Parameterized.Parameter(10)
+    public String documentIssue;
+
+    @Parameterized.Parameters(name = "{index}: Test with lastName={0} and name={1}")
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][]{
+                {"Greblov", "Sergey", "01011995","Греблов","Сергей","Олегович","01011995", "4614", "817781","15042016","Оуфмс России по Воронежской области" },
+                {"Vasilev", "Vasiliy", "27012002","Васильев","Василий","Васильевич","27012002", "4615", "717714","15042016","Киоск Союзпечать" }
+                ,{"Ivanov", "Ivan", "01021992","Иванов","Иван","Матвеевич","01021991", "4517", "647342","12052012","Продавец из электрички" }};
+        return Arrays.asList(data);
+    }
+
 
     @BeforeClass
     public static void testUp(){
         System.setProperty("webdriver.chrome.driver", "src/main/drivers/chromedriver");
         webDriver = new ChromeDriver();
-        webDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
     }
 
@@ -42,53 +81,57 @@ public class SberbankTest {
         WebElement button = webDriver.findElement(By.xpath("//a[contains(@class,'analytics-button')]"));
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", button);
         button.click();
-        webDriver.findElement(By.xpath("//div[@class='online-card-program selected']")).click();
-        webDriver.findElement(By.xpath("//button[text()='Оформить']")).click();
+        webDriver.findElement(By.xpath("//div[@class='online-card-program']/h3[contains(text(), 'Минимальная')]")).click();
+        WebElement buyButton =  webDriver.findElement(By.xpath("//button[text()='Оформить']"));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", buyButton);
+        new WebDriverWait(webDriver,5).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='Оформить']")));
+        buyButton.click();
+        WebElement elementSurname = webDriver.findElement(By.xpath("//input[@id='surname_vzr_ins_0']"));
+        WebElement elementName = webDriver.findElement(By.xpath("//input[@id='name_vzr_ins_0']"));
+        WebElement elementBirthdayDate = webDriver.findElement(By.xpath("//input[@id='birthDate_vzr_ins_0']"));
+        WebElement elementLastName = webDriver.findElement(By.xpath("//input[@id='person_lastName']"));
+        WebElement elementFirstName = webDriver.findElement(By.xpath("//input[@id='person_firstName']"));
+        WebElement elementMiddleName = webDriver.findElement(By.xpath("//input[@id='person_middleName']"));
+        WebElement elementPassportBirthdayDate = webDriver.findElement(By.xpath("//input[@id='person_birthDate']"));
+        WebElement elementPassportSeries = webDriver.findElement(By.xpath("//input[@id='passportSeries' and @placeholder='Серия']"));
+        WebElement elementPassportNumber = webDriver.findElement(By.xpath("//input[@id='passportNumber']"));
+        WebElement elementDocumentDate = webDriver.findElement(By.xpath("//input[@id='documentDate']"));
+        WebElement elementDocumentIssue = webDriver.findElement(By.xpath("//input[@id='documentIssue']"));
+
+        elementSurname.sendKeys(surname);
+        elementName.sendKeys(name);
+        elementBirthdayDate.sendKeys(birthdayDate);
+        elementLastName.sendKeys(Keys.CONTROL + "a");
+        elementLastName.sendKeys(lastName);
+        elementFirstName.sendKeys(firstName);
+        elementMiddleName.sendKeys(middleName);
+        elementPassportBirthdayDate.sendKeys(passportBirthdayDate);
+        elementPassportSeries.sendKeys(Keys.CONTROL + "a");
+        elementPassportSeries.sendKeys(passportSeries);
+        elementPassportNumber.sendKeys(passportNumber);
+        elementDocumentDate.sendKeys(documentDate);
+        elementDocumentIssue.sendKeys(Keys.CONTROL + "a");
+        elementDocumentIssue.sendKeys(documentIssue);
 
 
-        WebElement surname = webDriver.findElement(By.xpath("//input[@id='surname_vzr_ins_0']"));
-        WebElement name = webDriver.findElement(By.xpath("//input[@id='name_vzr_ins_0']"));
-        WebElement bDate = webDriver.findElement(By.xpath("//input[@id='birthDate_vzr_ins_0']"));
-        WebElement lName = webDriver.findElement(By.xpath("//input[@id='person_lastName']"));
-        WebElement fName = webDriver.findElement(By.xpath("//input[@id='person_firstName']"));
-        WebElement mName = webDriver.findElement(By.xpath("//input[@id='person_middleName']"));
-        WebElement pbDate = webDriver.findElement(By.xpath("//input[@id='person_birthDate']"));
-        WebElement passportSeries = webDriver.findElement(By.xpath("//input[@id='passportSeries' and @placeholder='Серия']"));
-        WebElement passportNumber = webDriver.findElement(By.xpath("//input[@id='passportNumber']"));
-        WebElement documentDate = webDriver.findElement(By.xpath("//input[@id='documentDate']"));
-        WebElement documentIssue = webDriver.findElement(By.xpath("//input[@id='documentIssue']"));
-
-        surname.sendKeys("Testov");
-        name.sendKeys("Test");
-        bDate.sendKeys("03031990");
-        lName.sendKeys(Keys.CONTROL + "a");
-        lName.sendKeys("Тестов");
-        fName.sendKeys("Тест");
-        mName.sendKeys("Тестович");
-        pbDate.sendKeys("03031990");
-        passportSeries.sendKeys(Keys.CONTROL + "a");
-        passportSeries.sendKeys("4516");
-        passportNumber.sendKeys("815562");
-        documentDate.sendKeys("04042016");
-        documentIssue.sendKeys(Keys.CONTROL + "a");
-        documentIssue.sendKeys("оуфмс россии по московской области");
-        Thread.sleep(4000);
-
-        Assert.assertEquals("Testov", surname.getAttribute("value"));
-        Assert.assertEquals("Test", name.getAttribute("value"));
-        Assert.assertEquals("03.03.1990", bDate.getAttribute("value"));
-        Assert.assertEquals("Тестов", lName.getAttribute("value"));
-        Assert.assertEquals("Сергей", fName.getAttribute("value"));
-        Assert.assertEquals("Тестович", mName.getAttribute("value"));
-        Assert.assertEquals("03.03.1990", pbDate.getAttribute("value"));
-        Assert.assertEquals("4516", passportSeries.getAttribute("value"));
-        Assert.assertEquals("815562", passportNumber.getAttribute("value"));
-        Assert.assertEquals("04.04.2016", documentDate.getAttribute("value"));
-        Assert.assertEquals("оуфмс россии по московской области", documentIssue.getAttribute("value"));
+        Assert.assertEquals(surname, elementSurname.getAttribute("value"));
+        Assert.assertEquals(name, elementName.getAttribute("value"));
+        Assert.assertEquals(formatDateWithDots(birthdayDate), elementBirthdayDate.getAttribute("value"));
+        Assert.assertEquals(lastName, elementLastName.getAttribute("value"));
+        Assert.assertEquals(firstName, elementFirstName.getAttribute("value"));
+        Assert.assertEquals(middleName, elementMiddleName.getAttribute("value"));
+        Assert.assertEquals(formatDateWithDots(passportBirthdayDate), elementPassportBirthdayDate.getAttribute("value"));
+        Assert.assertEquals(passportSeries, elementPassportSeries.getAttribute("value"));
+        Assert.assertEquals(passportNumber, elementPassportNumber.getAttribute("value"));
+        Assert.assertEquals(formatDateWithDots(documentDate), elementDocumentDate.getAttribute("value"));
+        Assert.assertEquals(documentIssue, elementDocumentIssue.getAttribute("value"));
 
         webDriver.findElement(By.xpath("//button[contains(text(), 'Продолжить')]")).click();
         Assert.assertTrue(webDriver.findElement(By.xpath("//div[@class='alert-form alert-form-error']")).isDisplayed());
 
 
+    }
+    public static String formatDateWithDots(String date){
+        return date.substring(0,2) + "." + date.substring(2,4) + "." + date.substring(4,8);
     }
 }
